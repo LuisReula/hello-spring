18f18a0b5a66cbf8e2da4f7116b261246ce85bc2
LuisReula <lorz@gft.com>
2022-01-31 12:41:53 +0100

    Added new Tests

--- src/main/java/com/sinensia/demo/DemoProjectApplication.java
+++ src/main/java/com/sinensia/demo/DemoProjectApplication.java
@@ -1,5 +1,6 @@
 package com.sinensia.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +16,24 @@ public class DemoProjectApplication {
 	}
 
 	@GetMapping("/")
-	public String Raiz(){
+	public String root(){
 		return String.format("Bienvenidos a mi primera prueba");
 	}
 
 	@GetMapping("/hello")
-	public String Hello(@RequestParam(value = "name", defaultValue = "World") String Name){
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String Name){
 		return String.format("Hello %s!", Name);
+	}
+
+	@GetMapping("/add")
+	public Object canAdd( @RequestParam(value="a", defaultValue="0") Float a,
+						@RequestParam(value="b", defaultValue="0") Float b){
+		Float sum =a+b;
+		Float decimals = sum - sum.intValue();
+		if (decimals!=0){
+			return sum;
+		}
+		return  Integer.valueOf(sum.intValue());
 	}
 
 }
--- src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
@@ -1,45 +1,210 @@
 package com.sinensia.demo;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.client.RestClientException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 class DemoProjectApplicationTests {
+
+	@Autowired TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	void rootTest(@Autowired TestRestTemplate restTemplate) {
+	void rootTest() {
 		assertThat(restTemplate.getForObject("/", String.class)).isEqualTo("Bienvenidos a mi primera prueba");
 	}
 
 	@Test
-	void rootTestHello(@Autowired TestRestTemplate restTemplate) {
+	void helloTest() {
 		assertThat(restTemplate.getForObject("/hello", String.class)).isEqualTo("Hello World!");
 	}
 
 	@Test
-	void rootTestHelloNombre(@Autowired TestRestTemplate restTemplate) {
+	void helloNameTest() {
 		assertThat(restTemplate.getForObject("/hello?name=Luis", String.class)).isEqualTo("Hello Luis!");
 	}
 
-	/*@Test
-	void rootTest(@Autowired WebTestClient webClient){
-		webClient
-				.get().uri("/hello")
-				.exchange()
-				.expectStatus().isOk()
-				.expectBody(String.class).isEqualTo("Hello World!");
-	}*/
+	@Test
+	void helloNames() {
+		String[] arr = {"Javier","Javier Arturo","Rodriguez"};
+		for(String name: arr) {
+			assertThat(restTemplate.getForObject("/hello?name="+name, String.class))
+					.isEqualTo("Hello "+name+"!");
+		}
+	}
 
+	@ParameterizedTest
+	@ValueSource(strings = {
+			"Luis",
+			"Luis Antonio",
+			"Antonio",
+			"Reula"
+	})
+	void helloParamNames(String name) {
+		assertThat(restTemplate.getForObject("/hello?name="+name, String.class))
+				.isEqualTo("Hello "+name+"!");
+	}
+
+	@DisplayName("test multiple input values")
+	@ParameterizedTest(name="[{index}] ({arguments}) \"{0}\" -> \"{1}\" ")
+	@CsvSource({
+			"a,            Hello a!",
+			"b,            Hello b!",
+			",             Hello null!",
+			"'',           Hello World!",
+			"' ',          Hello  !",
+			"first+last,   Hello first last!"
+	})
+	void helloParamNamesCsv(String name, String expected) {
+		assertThat(restTemplate.getForObject("/hello?name="+name, String.class))
+				.isEqualTo(expected);
+	}
+
+	@Test
+	void canAddTest(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1&b=2", String.class)).isEqualTo("3");
+	}
+
+	@Test
+	void canAddZeroTest(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=0&b=2", String.class)).isEqualTo("2");
+	}
+
+	@Test
+	void canAddNegativeTest(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1&b=-2", String.class)).isEqualTo("-1");
+	}
+
+	@Test
+	void canAddNullATest(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=&b=2", String.class)).isEqualTo("2");
+	}
+
+	@Test
+	void canAddNullBTest(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1&b=", String.class)).isEqualTo("1");
+	}
+
+	@Test
+	void canAddRealTest(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1&b=2.5", String.class)).isEqualTo("3.5");
+	}
+
+	@Test
+	void canAddMultiple(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/add?a=1&b=2", String.class))
+				.isEqualTo("3");
+		assertThat(restTemplate.getForObject("/add?a=0&b=2", String.class))
+				.isEqualTo("2");
+		assertThat(restTemplate.getForObject("/add?a=1&b=-2", String.class))
+				.isEqualTo("-1");
+		assertThat(restTemplate.getForObject("/add?a=&b=2", String.class))
+				.isEqualTo("2");
+		assertThat(restTemplate.getForObject("/add?a=1.5&b=2", String.class))
+				.isEqualTo("3.5");
+		assertThat(restTemplate.getForObject("/add?a=1&b=", String.class))
+				.isEqualTo("1");
+	}
+
+
+	@DisplayName("multiple additions")
+	@ParameterizedTest(name="[{index}] {0} + {1} = {2}")
+	@CsvSource({
+			"1,   2,   3",
+			"1,   1,   2",
+			"1.0, 1.0, 2",
+			"1,  -2,  -1",
+			"1.5, 2,   3.5",
+			"'',  2,   2",
+			"1.5, 1.5, 3"
+	})
+	void canAddCsvParameterized(String a, String b, String expected) {
+		assertThat(restTemplate.getForObject("/add?a="+a+"&b="+b, String.class))
+				.isEqualTo(expected);
+	}
+
+	@Test
+	void canAddExceptionJsonString() {
+		assertThat(restTemplate.getForObject("/add?a=string&b=1", String.class).indexOf("Bad Request"))
+				.isGreaterThan(-1);
+	}
+
+	@Test
+	void canAddFloat() {
+		assertThat(restTemplate.getForObject("/add?a=1.5&b=2", Float.class))
+				.isEqualTo(3.5f);
+	}
+
+	@Test
+	void canAddFloatException() {
+		Exception thrown = assertThrows(RestClientException.class, ()->{
+			restTemplate.getForObject("/add?a=hola&b=2", Float.class);
+		});
+	}
+
+
+	@DisplayName("multiple additions")
+	@ParameterizedTest(name="{displayName} [{index}] {0} + {1} = {2}")
+	@CsvSource({
+			"1,   2,   3",
+			"1,   1,   2",
+			"1.0, 1.0, 2",
+			"1,  -2,  -1",
+			"1.5, 2,   3.5",
+			"'',  2,   2",
+			"1.5, 1.5, 3"
+	})
+	void canAddCsvParameterizedFloat(String a, String b, String expected) {
+		assertThat(restTemplate.getForObject("/add?a="+a+"&b="+b, Float.class))
+				.isEqualTo(Float.parseFloat(expected));
+	}
+
+	/* Loss-of-precision by converting Float return value into Integer
+	@Test
+	void canAddInteger() {
+		assertThat(restTemplate.getForObject("/add?a=1.5&b=2", Integer.class))
+				.isEqualTo(3.5f);
+	}
+	*/
+
+
+	@Nested
+	@DisplayName(value="Application tests")
+	class AppTests {
+
+		@Autowired
+		private DemoProjectApplication app;
+
+		@Test
+		void appCanAddReturnsInteger() {
+			assertThat(app.canAdd(1f, 2f)).isEqualTo(3);
+		}
+
+		@Test
+		void appCanAddReturnsFloat() {
+			assertThat(app.canAdd(1.5f, 2f)).isEqualTo(3.5f);
+		}
+
+		@Test
+		void appCanAddNullReturnsFloat() {
+			assertThat(app.canAdd(null, 2f));	}
+	}
 }
 
